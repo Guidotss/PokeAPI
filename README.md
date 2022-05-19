c9260f7f9f762b92483bf03672c6ccf3253f5986
Guidotss <100441914+Guidotss@users.noreply.github.com>
2022-05-19 16:58:58 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Objetivo: 
+definir una API para gestionar nuestro propio equipo pokemon 
+
+##Acciones:
+-Identificarnos
+-Añadir pokemon a nuestro equipo
+-Eliminar pokemon de nuestro equipo
+-Intercambiar el orden de nuestros pokemons
+-Consultar informacion de nuestro equipo
+
+##REST Desing
+-Añadir pokemon: POST /team/pokemons
+-Consultar nuestro equipo: GET /team
+-Eliminar pokemon de nuestro equipo: DELETE /team/pokemon/:id
+-Intercambiar el orden de nuestros pokemons: PUT /team
+-Sistemas de credenciales
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const morgan = require('morgan'); 
+const app = express(); 
+const routerPokemon = require('./router/routerPokemons')
+
+app.use(express.urlencoded({extended:true})); 
+app.use(express.static(__dirname+'public'));
+app.use(express.json()); 
+app.use(morgan('dev')); 
+app.use('/team',routerPokemon); 
+
+
+
+const PORT = 8080; 
+
+const Server = app.listen(PORT,() =>{
+    console.log(`Server on por ${Server.address().port}`);
+})
+
+Server.on('error',err =>console.log(err)); 
+
+
+

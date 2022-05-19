5e73d334a10c32f2c14caaf7f2607cee10f4abd3
Guidotss <100441914+Guidotss@users.noreply.github.com>
2022-05-19 17:39:04 -0300

    creando rutas

--- router/routerPokemons.js
+++ router/routerPokemons.js
@@ -1,9 +1,21 @@
 const {Router} = require('express'); 
 const router = Router(); 
 
+router.get('/',(req,res)=>{
+    res.send('hola'); 
+})
 
+router.post('/pokemons',(req,res)=>{
 
+})
 
+router.put('/',(req,res)=>{
+
+})
+
+router.delete('/pokemons/:pokeId', (req,res)=>{
+
+})
 
 module.exports = router;
 
